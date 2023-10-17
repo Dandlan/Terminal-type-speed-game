dcaedfcf137d976c63a51d253ffbe7c5c0e5b253
Daniella <145188122+Dandlan@users.noreply.github.com>
2023-10-17 16:08:50 +0100

    added colours to the text

--- typing_game.py
+++ typing_game.py
@@ -2,8 +2,10 @@ import curses
 from curses import wrapper
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_WHITE)
+    curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_WHITE)
     stdscr.clear()
-    stdscr.addstr("Hello there")
+    stdscr.addstr("Hello there", curses.color_pair(1))
     stdscr.refresh()
     stdscr.getkey()
 
